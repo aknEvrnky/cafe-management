454788eb02131cdecd0d2946167f7fc29e3f3b80
aknEvrnky <evrenkaya.2002.save@gmail.com>
2025-02-08 17:01:25 +0300

    use vue toastification and listen session flash events

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,22 +1,50 @@
-<script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+<script>
+import {useToast} from "vue-toastification";
+
+export default {
+    name: "GuestLayout",
+    setup() {
+        const toast = useToast();
+
+        return {
+            toast,
+        }
+    },
+    watch: {
+        '$page.props.flash': {
+            handler (flash) {
+                if (flash.success) {
+                    this.toast.success(flash.success);
+                }
+                if (flash.error) {
+                    this.toast.error(flash.error);
+                }
+                if (flash.warning) {
+                    this.toast.warning(flash.warning);
+                }
+            },
+            deep: true,
+            immediate: true
+        },
+        '$page.props.errors': {
+            handler (errors) {
+                if (errors) {
+                    for (let key in errors) {
+                        this.toast.error(errors[key]);
+                    }
+                }
+            },
+            deep: true,
+            immediate: true
+        },
+    },
+}
 </script>
 
 <template>
-    <div
-        class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0"
-    >
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
-        </div>
-
-        <div
-            class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg"
-        >
-            <slot />
+    <div class="container mx-auto">
+        <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg mx-auto">
+            <slot/>
         </div>
     </div>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import Toast from "vue-toastification";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +20,9 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(Toast, {
+                position: "bottom-left",
+            })
             .mount(el);
     },
     progress: {
